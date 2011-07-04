27a2d657d9a7d4afe25a741aaf1c0bb6e59be985
Breno G. de Oliveira <garu@cpan.org>
2011-07-04 14:01:40 -0300

    initial commit

---
+++ lib/DataFlow/Proc/DBF.pm
@@ -0,0 +1,188 @@
+package DataFlow::Proc::DBF;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+use Moose;
+extends 'DataFlow::Proc::Converter';
+
+use XBase;
+use File::Temp qw(:seekable);
+use namespace::autoclean;
+
+has '+converter' => (
+    lazy    => 1,
+    default => sub { return XBase->new },
+    handles => {
+        dbf          => sub { shift->converter(@_)      },
+        dbf_opts     => sub { shift->converter_opts(@_) },
+        has_dbf_opts => sub { shift->has_converter_opts },
+    },
+    init_arg => 'dbf',
+);
+
+has '+converter_opts' => ( 'init_arg' => 'dbf_opts', );
+
+sub _policy {
+    return shift->direction eq 'CONVERT_TO' ? 'ArrayRef' : 'Scalar';
+}
+
+sub _build_subs {
+    my $self = shift;
+    return {
+        'CONVERT_TO' => sub {
+            die 'Not implemented (yet)';
+        },
+        'CONVERT_FROM' => sub {
+            my $string = shift;
+
+            my $options = $self->has_converter_opts
+                        ? $self->converter_opts : {}
+                        ;
+
+            my $dbf;
+
+            # if the user passes a file name or handle
+            # to read from, we use it. Otherwise, we
+            # assume the DBF is in a binary string 
+            # (the "flow") and make our interface with
+            #  XBase using a temp file
+            unless (exists $options->{'name'} or exists $options->{'fh'}) {
+                my $fh = File::Temp->new( UNLINK => 1 );
+                binmode $fh;
+                print $fh $string;
+                $fh->seek(0, SEEK_SET);
+
+                $options->{name} = '-';
+                $options->{fh}   = $fh;
+            }
+
+            $dbf = $self->converter->open( %$options )
+                or die XBase->errstr;
+
+            my $records = $dbf->get_all_records;
+            $dbf->close;
+            return $records;
+        },
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
+
+42;
+__END__
+
+=head1 NAME
+
+DataFlow::Proc::DBF - A dBase DBF converting processor
+
+
+=head1 SYNOPSIS
+
+    use DataFlow;
+
+    # simple flow
+    my $simple = DataFlow( 'DBF' );
+
+    # chained flow
+    my $chained = DataFlow([
+        ...     # (any procs you want in your flow)
+        'DBF',  # DataFlow::Proc::DBF
+        ...     # (any other procs you want in your flow)
+    ]);
+
+
+    # passing parameters
+    my $flow_with_args = DataFlow([
+       ...
+       [ 'DBF' => {
+           direction => 'CONVERT_FROM',
+         }
+       ],
+       ...
+    ]);
+
+  
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE 
+
+=for author to fill in:
+    Write a separate section listing the public components of the modules
+    interface. These normally consist of either subroutines that may be
+    exported, or methods that may be called on objects belonging to the
+    classes provided by the module.
+
+
+=head1 DIAGNOSTICS
+
+=over 4
+
+=item C<< Error message here, perhaps with %s placeholders >>
+
+[Description of error here]
+
+=back
+
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=for author to fill in:
+    A full explanation of any configuration system(s) used by the
+    module, including the names and locations of any configuration
+    files, and the meaning of any environment variables or properties
+    that can be set. These descriptions must also include details of any
+    configuration language used.
+  
+DataFlow::Proc::DBF requires no configuration files or environment variables.
+
+
+=head1 BUGS AND LIMITATIONS
+
+Please report any bugs or feature requests to
+C<bug-dataflow-proc-dbf@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+
+=head1 SEE ALSO
+
+L<DataFlow>
+
+=head1 AUTHOR
+
+Breno G. de Oliveira  C<< <garu@cpan.org> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2011, Breno G. de Oliveira C<< <garu@cpan.org> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
